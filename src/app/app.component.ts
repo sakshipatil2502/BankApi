b0ff29831037e14c75e8692e55d72431da563cce
sakshipatil2502 <sakhip1000@gamil.com>
2023-10-05 22:50:11 +0530

    Added first code to main branch

---
+++ src/app/app.component.ts
@@ -0,0 +1,31 @@
+/*import { Component } from '@angular/core';
+
+@Component({
+  selector: 'screen1',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'hello-angular';
+}*/
+
+import { Component, OnInit } from '@angular/core';
+import { User, UsersService } from './users.service';
+
+@Component({
+  selector: 'screen1',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit {
+  users: User[] = [];
+  constructor(private userService: UsersService) {}
+
+  ngOnInit(): void {
+    this.userService.getUsers().subscribe((data) => {
+      this.users = data;
+      console.log('DATA' , this.users);
+    });
+
+  }
+}
